14d2218f61360603ff85593d759cb136fba792c0
jbena4 <james.benadum@gmail.com>
2022-04-12 19:37:05 -0400

    added a data table and explanation of the project

--- app.py
+++ app.py
@@ -19,7 +19,16 @@ def run():
     predictions= svmmodel(signals_df)
     print(predictions)
     st.title('Team Five Project')
+    st.header('Bitcoin Machine Learning Model')
+    st.subheader('In this project, we built a machine learning model to trade bitcoin. We pulled data using the Alapaca API and calculated moving averages and certain bitcoin-specific indicators such as meyer multiples. We passed the dataframe into an SVM model from sklearn and results are presented on this page using streamlit.')
+    
+       
+    st.header('Cumulative Performance Line Chart')
     st.line_chart(predictions)
+
+
+    st.header('Predictions Dataframe')
+    st.table(predictions.head(20))
 
 
 
