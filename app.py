95e37fd9b1c46c25ec54bc731630963c7692f4c3
jbena4 <james.benadum@gmail.com>
2022-04-11 21:07:51 -0400

    updated streamlit chart

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from utils.tradingbot import signals_features
 from utils.svmmodel import svmmodel
-
+import streamlit as st
 
 ticker= "BTCUSD"
 url= "https://api.alternative.me/fng/?"
@@ -18,6 +18,8 @@ def run():
     signals_df= signals_features(ticker)
     predictions= svmmodel(signals_df)
     print(predictions)
+    st.title('Team Five Project')
+    st.line_chart(predictions)
 
 
 
